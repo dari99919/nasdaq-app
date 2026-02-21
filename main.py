957eede70272fe03e103ddf5729bb9be7a5d087d
dari99919 <dariramirez99@gmail.com>
2026-02-21 11:23:56 +0100

    cambio

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+from fastapi import FastAPI
+
+# Esto crea el servidor
+app = FastAPI()
+
+# Esto es lo que Render busca para saber que la web funciona
+@app.get("/")
+def home():
+    return {"mensaje": "Hola Mundo desde la API"}
+
+# Esta es la ruta que usará tu buscador de Lovable más tarde
+@app.get("/buscar")
+def buscar(query: str = ""):
+    return {"resultado": f"Has buscado: {query}"}
