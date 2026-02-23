fa384707850cf72d464666586803944d5cc836b6
dari99919 <105166946+dari99919@users.noreply.github.com>
2026-02-23 16:48:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+import streamlit as st
+
+# Esto SÍ se ve en la web
+st.title("¡Mi primera web!")
+st.write("Hola mundo, esta es mi página oficial.")
+
+# Esto NO se ve en la web (solo lo verías tú en los logs de Streamlit)
+print("Esto se queda en la consola")
